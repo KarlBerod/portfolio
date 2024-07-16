831b8faed696a60d58c64619e11f06d8b1d2467d
KarlBerod <karl.berod@gmail.com>
2024-07-16 13:14:19 +0200

    test

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -30,7 +30,7 @@ export default function ProjectPage({index}){
 
     return(
         <section className='project-page'>
-            <div className='project-left slider' style={{ backgroundImage: `url(${pictures[counter]})` }}>
+            <div className='project-left slider' style={{ backgroundImage: `url(${process.env.PUBLIC_URL + pictures[counter]})` }}>
                 <div className='arrows'>
                     <svg xmlns="http://www.w3.org/2000/svg" onClick={substractCounter} viewBox="0 0 448 512" className='slider-left-arrow'><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" onClick={addCounter} viewBox="0 0 448 512" className='slider-right-arrow'><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
